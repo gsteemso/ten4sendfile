a3a7a307b5aaa7cb2dde04bf92b1f203a402bcbe
Gordon Steemson <gsteemso@gmail.com>
2025-10-19 01:04:32 -0700

    Only define struct sf_hdtr if BUILDING_TEN4SENDFILE.

--- ten4sendfile.h
+++ ten4sendfile.h
@@ -1,13 +1,13 @@
-/* ten4sendfile:  Implements sendfile(), as missing from Mac OS 10.4.         *
+/* ten4sendfile:  Implements sendfile(2), as missing from Mac OS 10.4.        *
  *                                                                            *
  * Sendfile() appears in the Mac OS 10.4 system headers, but was not actually *
  * implemented.  (No manpage for it was provided, either).  This tiny library *
  * rectifies the former oversight.                                            */
 
-/* If this preprocessor constant is defined to anything else, the incorrect & *
- * unimplemented sendfile() system call prototype in the Mac OS 10.4.x system *
- * headers gets exposed.  We REALLY don't want that.                          */
-#define SENDFILE 0
+/* If this preprocessor constant is set true, the incorrect and unimplemented *
+ * sendfile() prototype in the Mac OS 10.4.x system headers gets exposed.  We *
+ * REALLY don't want that.                                                    */
+#undef SENDFILE
 
 /* This header file is meant to be installed, as <sys/socket.h>, to somewhere *
  * that gets searched for system headers before /usr/include.                 */
@@ -15,20 +15,26 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
-/* Per sys/uio.h, a {struct iovec} consists of a {void *iov_base} to a memory *
- * region and a {size_t iov_len} stating that region's length.  A {size_t} is *
- * an {unsigned long}.                                                        */
+/* Per sys/uio.h, a {struct iovec} consists of a {void *} to a memory region, *
+ * called "iov_base", & a {size_t} for its size, named "iov_len".  A {size_t} *
+ * is defined as an {unsigned long}.                                          */
 typedef struct iovec IOVec;
 
-/* This structure definition actually is already present in the Mac OS 10.4.x *
- * system headers - but it is only visible if the SENDFILE constant above has *
- * a nonzero definition, so we can't actually use it.                         */
+/* Per sys/socket.h, a {struct sf_hdtr} consists of four members, arranged as *
+ * a pair of array descriptions:  The first {IOVec *} is called "headers" and *
+ * its associated {int}, "hdr_cnt"; the second {IOVec *} is called "trailers" *
+ * and its {int} is "trlr_cnt".  Even though this structure definition should *
+ * by all rights be invisible because of the undefined constant above (not to *
+ * mention that _POSIX_C_SOURCE is probably defined at that point), placing a *
+ * copy here nevertheless SOMEHOW yields a duplicate-definition error.  HOW?! */
+#ifdef BUILDING_TEN4SENDFILE
 struct sf_hdtr {
     IOVec *headers;   /* Array of header data. */
       int  hdr_cnt;   /* Length of header array. */
     IOVec *trailers;  /* Array of trailer data. */
       int  trl_cnt;   /* Length of trailer array. */
 };
+#endif
 typedef struct sf_hdtr Sf_HdTr;
 
 int sendfile(int, int, off_t, off_t *, Sf_HdTr *, int);
